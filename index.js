252c50b5e7360bd201b3cb465da0b5af0c659b64
Ioniva <ioniva@icloud.com>
2023-01-17 13:55:36 +0200

    Add the configuration to read the .env file

--- index.js
+++ index.js
@@ -4,10 +4,16 @@
 import express from 'express';
 import http from 'http';
 import cors from 'cors';
+import dotenv from 'dotenv';
+import { fileURLToPath } from 'url';
+import path from 'path';
 
 /**
  * server configuration
  */
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+dotenv.config({ path: path.join(__dirname, '.env') });
 
 /**
  * express application
