2f05ee165ddf405d4e57be837f8bddb2c5574cfc
Ioniva <ioniva@icloud.com>
2023-01-17 13:55:06 +0200

    Remove console log

--- src/infraestructure/database/db.js
+++ src/infraestructure/database/db.js
@@ -1,13 +1,14 @@
 import pg from 'pg'
+
 const Client = pg.Client;
 
 const connect = async () => {
-
+    console.log(process.env.DB_HOST);
     const client = new Client({
-        database: "creditgo",
-        user: "postgres",
-        password: "admin123",
-        host: "127.0.0.1",
+        database: process.env.DB_NAME,
+        user: process.env.DB_USER,
+        password: process.env.DB_PASS.toString(),
+        host: process.env.DB_HOST,
         port: 5432
     });
 
