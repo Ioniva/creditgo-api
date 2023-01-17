8335a72da1708763818bae8b11ea9c93ae445140
Ioniva <ioniva@icloud.com>
2023-01-17 13:57:08 +0200

    Add the missing env properties to client object

--- src/infraestructure/database/db.js
+++ src/infraestructure/database/db.js
@@ -7,9 +7,9 @@ const connect = async () => {
     const client = new Client({
         database: process.env.DB_NAME,
         user: process.env.DB_USER,
-        password: process.env.DB_PASS.toString(),
+        password: process.env.DB_PASS,
         host: process.env.DB_HOST,
-        port: 5432
+        port: process.env.DB_PORT
     });
 
     await client.connect();
