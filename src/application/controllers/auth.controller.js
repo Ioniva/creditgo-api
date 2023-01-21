cb5076a40de3075868fb502405ee88d0759533cb
Ioniva <ioniva@icloud.com>
2023-01-21 07:17:49 +0200

    Add signin method

--- src/application/controllers/auth.controller.js
+++ src/application/controllers/auth.controller.js
@@ -1,17 +1,27 @@
 import db from '../../infraestructure/database/connection.js';
-import UserService from '../services/user.service.js'
+import AuthService from '../services/auth.service.js'
 
 // initializing
-const userService = new UserService();
+const authService = new AuthService();
 
 const signup = (async (req, resp, next) => {
     try {
         const userDTO = req.body;
-        await userService.createUser(userDTO);
-        resp.status(200).json("Se ha registrado correctamente!")
+        await authService.signup(userDTO);
+        resp.status(200).json("Se ha registrado correctamente!");
     } catch (error) {
         next(error);
     }
 })
 
-export { signup }
+const signin = (async (req, resp, next) => {
+    try {
+        const { email, password } = req.body;
+        const result = await authService.signin(email, password);
+        resp.status(200).json(result);
+    } catch (error) {
+        next(error);
+    }
+})
+
+export { signup, signin }
