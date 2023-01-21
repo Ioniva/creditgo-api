b127623c1da71a32d20732c06e92e925cc79cc83
Ioniva <ioniva@icloud.com>
2023-01-21 05:33:19 +0200

    Add user entity

---
+++ src/domain/entities/user.js
@@ -0,0 +1,14 @@
+class User {
+    constructor(id, email, password, created_at, updated_at, uuid) {
+        this.id = id;
+        this.email = email;
+        this.password = password;
+        this.created_at = created_at;
+        this.updated_at = updated_at;
+        this.uuid = uuid;
+
+        Object.seal(this); // prevent properties from being added or removed from the object.
+    }
+}
+
+export default User;
