bf695e3d76d77d61017094b7d82cc576acf97774
Ioniva <ioniva@icloud.com>
2023-01-21 11:30:46 +0200

    Add verify jwt middleware

--- src/infraestructure/routes/role.routes.js
+++ src/infraestructure/routes/role.routes.js
@@ -1,11 +1,12 @@
 import express from 'express';
 import { createRole, getAllRoles } from '../../application/controllers/role.controller.js';
 import validatorRoleDTO from '../middlewares/validation/role.validation.js';
+import verifyJWT from '../middlewares/verifyJWT.js';
 
 const router = express.Router();
 
-router.get('/', getAllRoles);
-router.post('/', validatorRoleDTO, createRole);
+router.get('/', verifyJWT, getAllRoles);
+router.post('/', validatorRoleDTO, verifyJWT, createRole);
 
 
 export default router;
