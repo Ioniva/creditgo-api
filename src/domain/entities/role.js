b9176ff2746b070ed3d247818ed508028fdddbcc
Ioniva <ioniva@icloud.com>
2023-01-21 05:33:01 +0200

    Add audit fields and protect the object

--- src/domain/entities/role.js
+++ src/domain/entities/role.js
@@ -3,6 +3,10 @@ class Role {
         this.id = id;
         this.name = name;
         this.code = code;
+        this.created_at = created_at;
+        this.updated_at = updated_at;
+
+        Object.seal(this); // prevent properties from being added or removed from the object.
     }
 }
 
