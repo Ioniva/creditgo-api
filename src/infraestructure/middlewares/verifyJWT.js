2e0500396fe1779740bfb9876394feb121784b82
Ioniva <ioniva@icloud.com>
2023-01-21 11:30:04 +0200

    Add JWT utility class

---
+++ src/infraestructure/middlewares/verifyJWT.js
@@ -0,0 +1,17 @@
+import JWTUtility from "../../domain/utilities/jwt.utilities.js";
+
+const verifyJWT = (req, resp, next) => {
+    try {
+        const token = req.headers.authorization;
+        if (!token) resp.status(401).json({ error: 'No token provided' })
+
+        const payload = JWTUtility.verify(token, process.env.JWT_SECRET);
+        req.user = payload;
+
+        next();
+    } catch (error) {
+        return resp.status(401).json({ error: 'Invalid token' });
+    }
+}
+
+export default verifyJWT;
