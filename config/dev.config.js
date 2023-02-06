2a8a477e7f3872a30e964be7f1c30a9677f7f962
Ioniva <ioniva@icloud.com>
2023-02-06 19:51:28 +0200

    Add multi environments config

---
+++ config/dev.config.js
@@ -0,0 +1,12 @@
+const dev = {
+  PORT: 4000,
+  DATABASE: {
+    NAME: 'db-name',
+    USER: 'db-access-user',
+    PASS: 'db-access-user-password',
+    HOST: '127.0.0.1',
+    PORT: 5432
+  }
+};
+
+export default dev;
---
+++ config/prod.config.js
@@ -0,0 +1,12 @@
+const prod = {
+  PORT: 6000,
+  DATABASE: {
+    NAME: 'db-name',
+    USER: 'db-access-user',
+    PASS: 'db-access-user-password',
+    HOST: '127.0.0.1',
+    PORT: 5432
+  }
+};
+
+export default prod;
