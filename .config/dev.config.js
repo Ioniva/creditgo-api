3f2e17f03347117bbad7dd8da364e99ab21320cb
Ioniva <ioniva@icloud.com>
2023-02-21 16:06:27 +0200

    Modify dev config

--- .config/dev.config.js
+++ .config/dev.config.js
@@ -1,9 +1,9 @@
 const dev = {
   PORT: 4000,
   DATABASEa: {
-    NAME: 'creditgo',
-    USER: 'postgres',
-    PASS: 'admin123',
+    NAME: 'db-name',
+    USER: 'db-access-user',
+    PASS: 'db-access-user-password',
     HOST: '127.0.0.1',
     PORT: 5432
   },
