0567dafc1156d37fb06e7084083fd490e42c7e0a
Ioniva <ioniva@icloud.com>
2023-02-26 13:47:57 +0200

    Small syntax correction

--- src/application/services/role.service.js
+++ src/application/services/role.service.js
@@ -1,13 +1,12 @@
 import Role from '../../domain/entities/role.js';
 import RoleMapper from '../mappers/role.mapper.js';
-
 const roleMapper = new RoleMapper();
 
 class RoleService {
   async getAllRoles () {
     try {
       const roles = await Role.findAll();
-      return roles.map(role => roleMapper.toDTO(role));
+      return roles.map((role) => roleMapper.toDTO(role));
     } catch (error) {
       return error;
     }
