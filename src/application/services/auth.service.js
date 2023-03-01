9df47de30e377524d3787e563b120a1dbb880d5f
Ioniva <ioniva@icloud.com>
2023-03-01 14:19:14 +0200

    Update auth import name

--- src/application/services/auth.service.js
+++ src/application/services/auth.service.js
@@ -1,19 +1,22 @@
 import http from 'http';
 import JWTUtility from '../utilities/jwt.utility.js';
 import PasswordUtility from '../utilities/password.utility.js';
-import User from '../../domain/entities/user_login_data.js';
+import LoginData from '../../domain/entities/login_data.js';
 import config from '../../../config/index.js';
 
 class AuthService {
   async signin ({ email, password }) {
-    const existingUser = await User.findOne({ where: { email } });
-    if (!existingUser) throw new Error('Invalid credentials', http.STATUS_CODES.UNAUTHORIZED);
+    const existingLoginData = await LoginData.findOne({ where: { email } });
+    if (!existingLoginData) throw new Error('Invalid credentials', http.STATUS_CODES.UNAUTHORIZED);
 
-    const isPasswordValid = await PasswordUtility.comparePasswords(password, existingUser.password);
+    const isPasswordValid = await PasswordUtility.comparePasswords(
+      password,
+      existingLoginData.password
+    );
     if (!isPasswordValid) throw new Error('Invalid credentials', http.STATUS_CODES.UNAUTHORIZED);
 
     const token = JWTUtility.sign(
-      { id: existingUser.uuid, email: existingUser.email },
+      { id: existingLoginData.uuid, email: existingLoginData.email },
       config.JWT_SECRET,
       { expiresIn: config.JWT_EXPIRATION }
     );
